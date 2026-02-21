9ed63b5a4bfc7dad086da4e2dbf28ea214a8186f
Adruva Team <info@adruva.com>
2026-02-21 20:14:51 +0530

    update static files

--- agency/settings.py
+++ agency/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'agency.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'core' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +124,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
